1ad39c91ab734509d5dcc357cb56eebe61d8c6ff
Fabien Stepho <stepho.fabien@gmail.com>
2018-08-22 17:45:58 +0200

    bootstrapModule switched to ngx-admin app-module

--- src/main/webapp/app/app.main.ts
+++ src/main/webapp/app/app.main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ProdConfig } from './blocks/config/prod.config';
-import { DashboardAppModule } from './app.module';
+import { AppModule } from './ngx-admin/app.module';
 
 ProdConfig();
 
@@ -9,6 +9,6 @@ if (module['hot']) {
 }
 
 platformBrowserDynamic()
-    .bootstrapModule(DashboardAppModule, { preserveWhitespaces: true })
+    .bootstrapModule(AppModule, { preserveWhitespaces: true })
     .then(success => console.log(`Application started`))
     .catch(err => console.error(err));
